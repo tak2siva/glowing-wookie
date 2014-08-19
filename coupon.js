6eaf4ab72f9b7e9485ec090fc08bdd0f7bb49ae9
Shiva <tak2siva@gmail.com>
2014-08-19 22:46:18 +0530

    Refactor -- remove template_* render only carousel

--- coupon.js
+++ coupon.js
@@ -62,6 +62,16 @@ Coupon.prototype.renderView = function(template_id) {
 }
 
 /*
+ * Requsted modifiction on 22/8 
+ * Ignore template_* and render only carousel
+ * Alternative for renderView() 
+ */
+Coupon.prototype.renderViewCarousel = function(json_data){
+    render_carousel(json_data);
+    add_banner();
+}
+
+/*
  * kind of static method to render home page
  */
 Coupon.renderHomePage = function() {
@@ -193,7 +203,8 @@ TerminalApp.init_web_socket_events = function(){
 
                 var coupon = new Coupon("container", jsonObj);
                 console.log("Rendering coupon page");
-                coupon.renderView();
+                //coupon.renderView();  Ignored template_* 
+                coupon.renderViewCarousel(coupon.data); // Render Carousel
 
                 TerminalApp.reset_payment_cron(); // Reset 20 sec timeout after every msg
             }
@@ -311,15 +322,16 @@ function add_banner(){
 $(function() {
     Coupon.renderHomePage();
 
-    init_testing_setup();
+   // init_testing_setup();
+
     $("#no_thanks").click(function(){
         TerminalApp.switchToPayment();
     })
 
     TerminalApp.init();
 
-    render_carousel(test_data);
-    add_banner();
+    //render_carousel(test_data); // test_data obtained from test_data.js for sample
+    //add_banner();
 });
 
 
