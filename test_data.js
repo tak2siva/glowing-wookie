cd8ba22c2425f64d7eb2836c2b0548f051e52743
Shiva <tak2siva@gmail.com>
2014-08-19 14:49:17 +0530

    Additional files for carousel implementation

---
+++ test_data.js
@@ -0,0 +1,92 @@
+ var test_data = {
+
+"notification_type":"PUB",
+
+"merchant_uid":"vshopinc",
+
+"merchant_major_location":"53727",
+
+"merchant_minor_location":"43210",
+
+"terminal_message":{
+
+"customer_name":"John Dee",
+
+"customer_id":1,
+
+"no_of_coupons":3,
+
+"coupons":[
+
+{
+
+"coupon_id":7,
+
+"coupon_name":"Extra Gemcrazy level",
+
+"coupon_code":"GC-2789",
+
+"coupon_description":"Get the exclusive Gemcrazy “Opalcrazy” levels half off!",
+
+"coupon_percentage":"50%",
+
+"coupon_start_date":"07/15/2014",
+
+"coupon_end_date":"07/15/2015",
+
+"coupon_image":"http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg",
+
+"barcode_image":"iVBORw0KGgoAAAANSUhEUg....."
+
+},
+
+{
+
+"coupon_id":8,
+
+"coupon_name":"$7 off Blockstacker ",
+
+"coupon_code":"TTRSCLN8912",
+
+"coupon_description":"Can you overcome the XTREME speed blocks? Get $7 off your purchase of the Blockstacker “XTREME” downloadable content!",
+
+"coupon_percentage":"$10",
+
+"coupon_start_date":"07/15/2014",
+
+"coupon_end_date":"07/15/2015",
+
+"coupon_image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_O_m4sxFWHoTTMPmxMBouJmlag-3hkPr4KZfn9Pkb3eJgOhFP",
+
+"barcode_image":"iVBORw0KGgoAAAANSUhEUg....."
+
+},
+
+{
+
+"coupon_id":9,
+
+"coupon_name":"10 Extra lives in Ge",
+
+"coupon_code":"GC-9852",
+
+"coupon_description":"Gems too crazy for you? Get 10 extra lives free in Gemcrazy with this exclusive Vshop offer!",
+
+"coupon_percentage":"10 lives",
+
+"coupon_start_date":"07/15/2014",
+
+"coupon_end_date":"07/15/2015",
+
+"coupon_image":"http://www.gettyimages.in/CMS/StaticContent/1391099215267_hero2.jpg",
+
+"barcode_image":"iVBORw0KGgoAAAANSUhEUg....."
+
+}
+
+]
+
+}
+
+};
+
